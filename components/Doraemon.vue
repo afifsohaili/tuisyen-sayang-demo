9dbbade8967b91536d1f47deadf2c19e55c9c659
Afif Sohaili <afifnajib@gmail.com>
2017-10-27 10:55:55 +0800

    Implement event.

--- components/Doraemon.vue
+++ components/Doraemon.vue
@@ -30,7 +30,7 @@ export default {
 
 <style scoped>
 .cartoon{
-  position: relative;
+  position: absolute;
 }
 
 .head{
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div v-on:keypress="move">
     <SirAmin/>
-    <Doraemon />
+    <Doraemon :top="topPos" :left="leftPos"/>
   </div>
 </template>
 
@@ -10,9 +10,43 @@ import SirAmin from '~/components/SirAmin'
 import Doraemon from '~/components/Doraemon'
 
 export default {
+  created: function () {
+    if (!process.server) {
+      window.addEventListener('keydown', this.move)
+    }
+  },
   components: {
     SirAmin,
     Doraemon
+  },
+  data: function () {
+    return {
+      left: 100,
+      top: 50
+    }
+  },
+  computed: {
+    leftPos: function () { return this.left + 'px' },
+    topPos: function () { return this.top + 'px' }
+  },
+  methods: {
+    move: function (e) {
+      if (e.key === 'ArrowUp') {
+        e.preventDefault()
+        this.top = this.top - 5
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault()
+        this.top = this.top + 5
+      } else if (e.key === 'ArrowLeft') {
+        e.preventDefault()
+        this.left = this.left - 5
+      } else if (e.key === 'ArrowRight') {
+        e.preventDefault()
+        this.left = this.left + 5
+      }
+
+      console.log(this.top, this.left)
+    }
   }
 }
 </script>
